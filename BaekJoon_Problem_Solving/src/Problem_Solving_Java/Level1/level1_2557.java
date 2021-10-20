9166ae017623df39c47ab73461b6345ff4894b9e
user <user@DESKTOP-7PO26L5>
2021-10-20 11:29:52 +0900

    2021.10.20 문제풀이

---
+++ BaekJoon_Problem_Solving/src/Problem_Solving_Java/Level1/level1_2557.java
@@ -0,0 +1,15 @@
+package Problem_Solving_Java.Level1;
+
+// 시간 : 2021-10-20 10:06 ~ 10:06
+
+// 문제 : Hello World!를 출력하시오
+
+public class level1_2557 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
---
+++ BaekJoon_Problem_Solving/src/Problem_Solving_Java/Level1/level2_10718.java
@@ -0,0 +1,16 @@
+package Problem_Solving_Java.Level1;
+
+// 시간 : 2021-10-20 10:10 ~ 10:11
+
+// 문제 : 두 줄에 걸쳐 "강한친구 대한육군"을 한 줄에 한 번식 출력한다.
+
+public class level2_10718 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+
+	}
+
+}
