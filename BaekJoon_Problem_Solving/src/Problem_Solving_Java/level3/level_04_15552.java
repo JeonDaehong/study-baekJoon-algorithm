c7b4a6d71175a61cae74c578e735483d0affd731
user <user@DESKTOP-7PO26L5>
2021-10-23 20:41:04 +0900

    23일 문제풀이

--- BaekJoon_Problem_Solving/src/Problem_Solving_Java/level3/level_04_15552.java
+++ BaekJoon_Problem_Solving/src/Problem_Solving_Java/level3/level_04_15552.java
@@ -1,7 +1,13 @@
 package Problem_Solving_Java.level3;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-//시간 : 2021-10-22 10:35 ~ 10:
+//시간 : 2021-10-23 20:35 ~ 20:39
 
 /*
 문제 : 본격적으로 for문 문제를 풀기 전에 주의해야 할 점이 있다.
@@ -22,8 +28,27 @@ public class level_04_15552 {
 
 	public static void main(String[] args) {
 		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		try {
+		
+			int T = Integer.parseInt(br.readLine());
+			
+			StringTokenizer st;
+			
+			for (int i=0; i<T; i++) {
+				st = new StringTokenizer(br.readLine(), " ");
+				bw.write(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + "\n");
+			}
+			
+			bw.flush();
 
-
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {br.close();} catch (IOException e) {e.printStackTrace();}
+			try {bw.close();} catch (IOException e) {e.printStackTrace();}
+		}
 	}
-
 }
